5634f9b8364e75e2df4aaf5b6ff7d136f16a07ef
qqliu10 <512174989@qq.com>
2016-12-09 09:29:40 +0800

    修改user.tumblr.com的检测逻辑

--- app/src/main/assets/user_tumblr_com_iframe_detector.js
+++ app/src/main/assets/user_tumblr_com_iframe_detector.js
@@ -1,9 +1,7 @@
 var USER_TUMBLR_COM_URL_REG = "http://[^www].*.tumblr.com/.*";
 
-//探测合适的iframe
-registerIFrameDetectFunction(USER_TUMBLR_COM_URL_REG, function () {
-    var iframeArray = new Array;
-
+//通过内容区收集iframe
+function detectIframeByContent(iframeArray) {
     var contentSectionArray = document.getElementsByClassName("content clearfix avatar-style-square show-nav");
     log("detect in user.tumblr.com, found content= " + contentSectionArray.length);
     if(contentSectionArray) {
@@ -25,29 +23,107 @@ registerIFrameDetectFunction(USER_TUMBLR_COM_URL_REG, function () {
             }
         }
     }
+}
+
+//通过视频容器区收集iframe
+function detectIframeByVideoContainer(iframeArray) {
+    var videoContainers = document.getElementsByClassName("tumblr_video_container");
+    if(!videoContainers) {
+        return;
+    }
+
+    for(var i = 0 ; i < videoContainers.length; i++) {
+        var itemVideoContainer = videoContainers[i];
+        var itemIframes = itemVideoContainer.getElementsByTagName("iframe");
+        if(!itemIframes) {
+            continue;
+        }
+
+        for(var j = 0; j < itemIframes.length; j++) {
+            var iframePart = itemIframes[j];
+            iframeArray.push(iframePart);
+        }
+    }
+}
+
+//探测合适的iframe
+registerIFrameDetectFunction(USER_TUMBLR_COM_URL_REG, function () {
+    var iframeArray = new Array;
+
+    //通过内容区定位iframe
+    detectIframeByContent(iframeArray);
+
+    //通过视频容器区收集iframe
+    detectIframeByVideoContainer(iframeArray);
 
     return iframeArray;
 });
 
-//探测资源
-registerResourceDetectFunction(USER_TUMBLR_COM_URL_REG, function () {
+//检测页面内的图集
+function detectPicSet() {
     //探测图片
 	var picElements = document.getElementsByClassName("photoset_photo");
 	if(!picElements) {
-		log("detectPics() picElements is undefined");
+		log("detectPicSet() picElements is undefined");
 		return;
 	}
-	log("detectPics() found pic elements count= " + picElements.length);
+
+	log("detectPicSet() found pic elements count= " + picElements.length);
 
 	for (var i= 0; i < picElements.length; i++) {
 		var picSrc = picElements[i].dataset.src;
-//		log("detectPics() found pic elements: " + picSrc);
 
 		if(resourceDetectorListener) {
 			resourceDetectorListener.onDetected("pic", document.title, picSrc, null, null);
 		}
 	}
+}
+
+//检测页面内只有一张图片的场景
+function detectOnePic() {
+    var contentSections = document.getElementsByClassName("content clearfix  avatar-style-square  show-nav");
+    if(!contentSections) {
+        return;
+    }
+
+    for(var i = 0;i < contentSections.length; i++) {
+        var contentSection = contentSections[i];
+
+        var photoWrapperInnerDivs = contentSection.getElementsByClassName("photo-wrapper-inner");
+
+        if(!photoWrapperInnerDivs) {
+            continue;
+        }
+
+        for(var j = 0;j < photoWrapperInnerDivs.length; j++) {
+            var photoWrapperInnerDiv = photoWrapperInnerDivs[j];
+
+            var imgDivs = photoWrapperInnerDiv.getElementsByTagName("img");
+
+            if(!imgDivs) {
+                continue;
+            }
+
+            for(var k = 0;k < imgDivs.length; k++) {
+                var imgDiv = imgDivs[k];
+                var picSrc = imgDiv.src;
+
+                if(resourceDetectorListener) {
+                    resourceDetectorListener.onDetected("pic", document.title, picSrc, null, null);
+                }
+            }
+        }
+    }
+}
+
+//探测资源
+registerResourceDetectFunction(USER_TUMBLR_COM_URL_REG, function () {
+    //探测图集
+    detectPicSet();
 
 	//探测视频
 	detectVideoByVideoTag();
+
+	//探测单张图片
+	detectOnePic();
 });
